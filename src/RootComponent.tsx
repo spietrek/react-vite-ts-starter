284a62534ec60fe3354914d9aff9192ea5c7f2e4
spietrek <spietrek@gmail.com>
2022-02-24 11:51:06 -0500

    lazy load route imports

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,17 +1,43 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import StoreCounterPage from './pages/StoreCounterPage'
-import StoreTodosPage from './pages/StoreTodosPage'
-import NotFoundPage from './pages/NotFoundPage'
+// import StoreCounterPage from './pages/StoreCounterPage'
+// import StoreTodosPage from './pages/StoreTodosPage'
+// import NotFoundPage from './pages/NotFoundPage'
 import './styles/index.css'
+
+const StoreCounterPage = lazy(() => import('./pages/StoreCounterPage'))
+const StoreTodosPage = lazy(() => import('./pages/StoreTodosPage'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'))
 
 const App = (): JSX.Element => (
   <BrowserRouter>
     <Routes>
-      <Route path="*" element={<NotFoundPage />} />
+      <Route
+        path="*"
+        element={
+          <Suspense fallback={<>...</>}>
+            <NotFoundPage />
+          </Suspense>
+        }
+      />
       <Route path="/" element={<HomePage />} />
-      <Route path="/counter" element={<StoreCounterPage />} />
-      <Route path="/todos" element={<StoreTodosPage />} />
+      <Route
+        path="/counter"
+        element={
+          <Suspense fallback={<>...</>}>
+            <StoreCounterPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/todos"
+        element={
+          <Suspense fallback={<>...</>}>
+            <StoreTodosPage />
+          </Suspense>
+        }
+      />
     </Routes>
   </BrowserRouter>
 )
