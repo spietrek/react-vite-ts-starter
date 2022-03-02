08144369f40ce4c7b8d34801626348adb218f0c1
spietrek <spietrek@gmail.com>
2022-03-02 09:43:27 -0500

    add navbar

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,9 +1,7 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ShellWrapper from 'components/organisms/ShellWrapper'
 import HomePage from './pages/HomePage'
-// import StoreCounterPage from './pages/StoreCounterPage'
-// import StoreTodosPage from './pages/StoreTodosPage'
-// import NotFoundPage from './pages/NotFoundPage'
 import './styles/index.css'
 
 const StoreCounterPage = lazy(() => import('./pages/StoreCounterPage'))
@@ -12,43 +10,45 @@ const ParentsPage = lazy(() => import('./pages/ParentsPage'))
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage'))
 
 const App = (): JSX.Element => (
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="*"
-        element={
-          <Suspense fallback={<>...</>}>
-            <NotFoundPage />
-          </Suspense>
-        }
-      />
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/counter"
-        element={
-          <Suspense fallback={<>...</>}>
-            <StoreCounterPage />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/todos"
-        element={
-          <Suspense fallback={<>...</>}>
-            <StoreTodosPage />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/parents"
-        element={
-          <Suspense fallback={<>...</>}>
-            <ParentsPage />
-          </Suspense>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+  <ShellWrapper>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<>...</>}>
+              <NotFoundPage />
+            </Suspense>
+          }
+        />
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/counter"
+          element={
+            <Suspense fallback={<>...</>}>
+              <StoreCounterPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/todos"
+          element={
+            <Suspense fallback={<>...</>}>
+              <StoreTodosPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/parents"
+          element={
+            <Suspense fallback={<>...</>}>
+              <ParentsPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </ShellWrapper>
 )
 
 export default App
