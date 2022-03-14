f307c1fc7b2de0ea8940f2288c0d4a1775c1966f
spietrek <spietrek@gmail.com>
2022-03-14 09:27:05 -0400

    add example markup

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const SettingsPage = (): JSX.Element => {
   return (
-    <div>
-      <h1>Settings Page</h1>
+    <div className="tw-container tw-mx-auto">
+      <div className="tw-flex tw-min-h-screen tw-items-center tw-justify-center">
+        <div className="tw-w-[900px] tw-rounded-md tw-bg-slate-300 tw-py-16 tw-px-4 tw-text-center tw-shadow-3xl">
+          <Link className="tw-btn tw-btn-sm tw-mb-4" to="/">
+            Home
+          </Link>
+
+          <h1>Settings</h1>
+        </div>
+      </div>
     </div>
   )
 }
