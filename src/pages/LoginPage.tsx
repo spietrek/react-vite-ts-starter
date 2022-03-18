acbea4a50caa179e09e16c798fab71f142842ceb
spietrek <spietrek@gmail.com>
2022-03-18 14:12:57 -0400

    remove unused code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import { useState, useEffect } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useAuth } from '@/hooks/useAuth'
 import { useLocationPathName } from '@/hooks/useLocationPathName'
 
