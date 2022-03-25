6522303bb66d5025ee98d1d481ed30dd3c02c357
spietrek <spietrek@gmail.com>
2022-03-25 11:23:40 -0400

    cleanup page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const NotFoundPage = (): JSX.Element => {
   return (
-    <section>
-      <h1>Oops 404!</h1>
+    <section className="tw-container tw-mx-auto">
+      <div className="tw-flex tw-min-h-screen tw-items-center tw-justify-center">
+        <div className="tw-w-[900px] tw-rounded-md tw-bg-slate-300 tw-py-16 tw-px-4 tw-text-center tw-shadow-3xl">
+          <Link className="tw-btn tw-btn-sm tw-mb-4" to="/">
+            Home
+          </Link>
+
+          <h1>Oops! Page cannot be found.</h1>
+        </div>
+      </div>
     </section>
   )
 }
